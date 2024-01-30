0a11635b8306a641437c71f7e4a0b87e4f1438d7
JuniaMCampos <juniacamposjc@gmail.com>
2024-01-29 21:42:46 -0300

    Minha solução

--- index.py
+++ index.py
@@ -1,3 +1,71 @@
+import os
+
+def clear_screen():
+    os.ssystem('cls' if os.name == 'nt' else 'clear')
+
+while True:
+
+    print(" 0: Soma")
+    print(" 1: Subtração")
+    print(" 2: Multiplicação")
+    print(" 3: Divisão")
+    print(" 4: Exponenciação")
+
+    print("Escolha a operação que deseja realizar: ")
+
+    operacao = int(input())
+
+    if operacao == 0:
+        print("Você escolheu Soma")
+        print("Digite um número: ")
+        n1 = int(input())
+        print("Digite outro número: ")
+        n2 = int(input())
+        print("A soma de {} e {} é: {} ".format(n1, n2, n1 + n2))    
+
+    elif operacao == 1:
+        print("Você escolheu Subtração")
+        print("Digite um número: ")
+        n1 = int(input())
+        print("Digite outro número: ")
+        n2 = int(input())
+        print("A subtração de {} e {} é: {} ".format(n1, n2, n1 - n2))  
+
+    elif operacao == 2:
+        print("Você escolheu Multiplicação")
+        print("Digite um número: ")
+        n1 = int(input())
+        print("Digite outro número: ")
+        n2 = int(input())
+        print("A multiplicação de {} e {} é: {} ".format(n1, n2, n1 * n2))  
+
+    elif operacao == 3:
+        print("Você escolheu Divisão")
+        print("Digite um número: ")
+        n1 = int(input())
+        print("Digite outro número: ")
+        n2 = int(input())
+        print("A divisão de {} e {} é: {} ".format(n1, n2, n1 / n2))  
+
+    elif operacao == 4:
+        print("Você escolheu exponenciação")
+        print("Digite a base: ")
+        n1 = int(input())
+        print("Digite o expoente: ")
+        n2 = int(input())
+        print("{} elevado a {} é: {} ".format(n1, n2, n1 ** n2))  
+    
+    else:
+        print("Operação não reconhecida")
+
+    print("Deseja fazer outra operação? 0 - Sim, 1 - Não ")
+    resposta = int(input())
+
+    if resposta != '0':
+        break
+    else:
+        clear_screen()
+
+#Não consegui fazer voltar pro looping depois que o usuario aperta 0
 
 
-print("Hello world")
